e5ac14278a2fde8d8ebd274a1c571be88ced93fa
quiquelhappy <quiquelhappy@gmail.com>
2024-05-09 14:16:03 +0200

    misc

--- machine/container/Settings.go
+++ machine/container/Settings.go
@@ -3,7 +3,6 @@ package container
 type Settings struct {
 	Ports   map[int]int       `json:"ports"`
 	Envs    map[string]string `json:"envs"`
-	Volume  string            `json:"volume"`
 	Path    string            `json:"path"`
 	Ip      string            `json:"ip"`
 	Memory  int               `json:"memory"`
